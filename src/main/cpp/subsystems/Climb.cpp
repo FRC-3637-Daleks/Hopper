7995a607a83d3487b8ccc6c98b7f2756b1604ec5
SamSri01 <109153482+GuyPersonMan01@users.noreply.github.com>
2024-02-05 18:29:41 -0500

    Update Climb.cpp

--- src/main/cpp/subsystems/Climb.cpp
+++ src/main/cpp/subsystems/Climb.cpp
@@ -1,16 +1,49 @@
 // ClimbSubsystem.cpp
-
+// object name = digitalInput
 #include "subsystems/Climb.h"
+#include <frc2/command/ConditionalCommand.h>
+#include <frc2/command/Commands.h>
+
+frc2::CommandPtr Climb::ExtendClimb() {
+    //return Run([this] {m_climbMotor.Set(1);});
 
 
-void Climb::ExtendClimb() {
-    m_climbMotor.Set(1);
+    return frc2::cmd::RunEnd([this] {
+            m_climbMotor.Set(1);
+        }, [this] () {
+             m_climbMotor.Set(0);
+        }, {this})
+        .Until([this] () -> bool {
+            return (m_climbBottom.Get() && m_climbTop.Get());
+        });
+
+    // return frc2::ConditionalCommand(
+    //     frc2::cmd::Run([this] {m_climbMotor.Set(1);}),
+    //     frc2::cmd::Run([this] {m_climbMotor.Set(0);}),
+    //     [this] () -> bool {return (m_climbBottom.Get() && m_climbTop.Get());}
+    // ).ToPtr();
+
 }
 
-void Climb::RetractClimb() {
-    m_climbMotor.Set(-1);
+frc2::CommandPtr Climb::RetractClimb() {
+    //return Run([this] {m_climbMotor.Set(-1);});
+    return frc2::cmd::RunEnd([this] {
+            m_climbMotor.Set(-1);
+        }, [this] () {
+             m_climbMotor.Set(0);
+        }, {this})
+        .Until([this] () -> bool {
+            return (m_climbBottom.Get() && m_climbTop.Get());
+        });
+
+    // return frc2::ConditionalCommand(
+    //                                 frc2::CommandPtr([this] {m_climbMotor.Set(-1);}),
+    //                                 frc2::CommandPtr([this] {m_climbMotor.Set(0);}),
+    //                                 [this] () -> bool {return (m_climbBottom.Get() && m_climbTop.Get());});
+                        
 }
 
-void Climb::StopClimb() {
-    m_climbMotor.Set(0);
-}
+//frc2::CommandPtr Climb::StopClimb() {
+//    return Run([this] {m_climbMotor.Set(0);});
+//}
+
