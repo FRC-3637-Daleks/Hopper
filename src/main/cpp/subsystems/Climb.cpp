dc6f7cf0c8b072d6f73a80bbb14c57c564a0dcb1
SamSri01 <109153482+GuyPersonMan01@users.noreply.github.com>
2024-02-10 10:03:25 -0500

    Updated SmartDashboard to have the motor speeds and state of the sensors

--- src/main/cpp/subsystems/Climb.cpp
+++ src/main/cpp/subsystems/Climb.cpp
@@ -5,12 +5,18 @@
 #include "subsystems/Climb.h"
 #include <frc2/command/ConditionalCommand.h>
 #include <frc2/command/Commands.h>
+#include <frc/smartdashboard/SmartDashboard.h>
 
 frc2::CommandPtr Climb::ClimbCommand(double input) {
+    frc::SmartDashboard::PutNumber("Climb Input: ", input);
+    frc::SmartDashboard::PutBoolean("Climb Top Limit: ", m_climbTop.Get());
+    frc::SmartDashboard::PutBoolean("Climb Bottom Limit: ", m_climbBottom.Get());
     return frc2::cmd::RunEnd([this, input] {
             m_climbMotor.Set(input);
         }, [this] () {
              m_climbMotor.Set(0);
+             frc::SmartDashboard::PutBoolean("Climb Top Limit: ", m_climbTop.Get());
+             frc::SmartDashboard::PutBoolean("Climb Bottom Limit: ", m_climbBottom.Get());
         }, {this})
         .Until([this] () -> bool {
             return (m_climbBottom.Get() || m_climbTop.Get());
@@ -20,16 +26,19 @@ frc2::CommandPtr Climb::ClimbCommand(double input) {
 frc2::CommandPtr Climb::ExtendClimb() {
     //return Run([this] {m_climbMotor.Set(1);});
 
-
+    frc::SmartDashboard::PutNumber("Climb Input: ", 1);
+    frc::SmartDashboard::PutBoolean("Climb Top Limit: ", m_climbTop.Get());
+    frc::SmartDashboard::PutBoolean("Climb Bottom Limit: ", m_climbBottom.Get());
     return frc2::cmd::RunEnd([this] {
             m_climbMotor.Set(1);
         }, [this] () {
              m_climbMotor.Set(0);
+             frc::SmartDashboard::PutBoolean("Climb Top Limit: ", m_climbTop.Get());
+             frc::SmartDashboard::PutBoolean("Climb Bottom Limit: ", m_climbBottom.Get());
         }, {this})
         .Until([this] () -> bool {
             return (m_climbBottom.Get() || m_climbTop.Get());
         });
-
     // return frc2::ConditionalCommand(
     //     frc2::cmd::Run([this] {m_climbMotor.Set(1);}),
     //     frc2::cmd::Run([this] {m_climbMotor.Set(0);}),
@@ -40,10 +49,15 @@ frc2::CommandPtr Climb::ExtendClimb() {
 
 frc2::CommandPtr Climb::RetractClimb() {
     //return Run([this] {m_climbMotor.Set(-1);});
+    frc::SmartDashboard::PutNumber("Climb Input: ", -1);
+    frc::SmartDashboard::PutBoolean("Climb Top Limit: ", m_climbTop.Get());
+    frc::SmartDashboard::PutBoolean("Climb Bottom Limit: ", m_climbBottom.Get());
     return frc2::cmd::RunEnd([this] {
             m_climbMotor.Set(-1);
         }, [this] () {
              m_climbMotor.Set(0);
+             frc::SmartDashboard::PutBoolean("Climb Top Limit: ", m_climbTop.Get());
+             frc::SmartDashboard::PutBoolean("Climb Bottom Limit: ", m_climbBottom.Get());
         }, {this})
         .Until([this] () -> bool {
             return (m_climbBottom.Get() || m_climbTop.Get());
